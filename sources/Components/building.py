c9ef48751df9bef7dadd6bd3f4fdbfbab67e07dc
CasparHadley1 <caspar.hadley@icloud.com>
2025-01-30 17:26:06 +0000

    createFloors

--- sources/Components/building.py
+++ sources/Components/building.py
@@ -1,16 +1,21 @@
 # building components
 
-import floor
-import person
+from floor import *
+from person import *
 class building:
-    def __init__(self,floors,liftCapacity,requests):
-        self.__numOfFloors = floors
+    def __init__(self,floorsNum,liftCapacity,requests):
+        self.__numOfFloors = floorsNum
         self.__capacity = liftCapacity
         self.__requests = requests
         self.__people = []
+        self.__floors = []
 
     def __createPeople(self) -> None:
         for i in self.__requests:
-            newPerson = person.Person(i)
+            newPerson = Person(i)
             self.__people.append(newPerson)
+
+    def __createFloors(self) -> None:
+        for i in range(numOfFloors):
+            self.__floors.append(Floor(i))
     

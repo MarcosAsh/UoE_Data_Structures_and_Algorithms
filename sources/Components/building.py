ca25bf7aaab3093af58d4677e6e40383af77091f
CasparHadley1 <caspar.hadley@icloud.com>
2025-02-02 19:12:59 +0000

    distribute people to floors

--- sources/Components/building.py
+++ sources/Components/building.py
@@ -4,17 +4,21 @@ from floor import *
 from person import *
 from lift import *
 class building:
-    def __init__(self,floorsNum,liftCapacity,requests):
+    def __init__(self,floorsNum,liftCapacity,requests,currentLocations):
         self.__numOfFloors = floorsNum
         self.__capacity = liftCapacity
         self.__requests = requests
         self.__people = []
         self.__floors = []
+        self.__currentLocations = currentLocations
 
     def __createPeople(self) -> None:
         for i in self.__requests:
-            newPerson = Person(i)
-            self.__people.append(newPerson)
+            currentFloor = self.GetFloor(i)
+            for j in i:
+                newPerson = Person(j)
+                self.__people.append(newPerson)
+                currentFloor.AddToPeople(newPerson)
 
     def __createFloors(self) -> None:
         for i in range(self.__numOfFloors):
@@ -22,3 +26,8 @@ class building:
     
     def __createLift(self) -> None:
         self.__lift = lift(0,False,False,1,self.__capacity)
+
+    def GetFloor(self,index) -> Floor:
+        return self.__floors[index]
+
+

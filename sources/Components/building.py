3350ba399b38e50ffab6411922a5750776811c90
CasparHadley1 <caspar.hadley@icloud.com>
2025-02-02 19:28:14 +0000

    Updated constructor and added docstrings

--- sources/Components/building.py
+++ sources/Components/building.py
@@ -13,7 +13,14 @@ class building:
         self.__floors = []
         self.__currentLocations = currentLocations
 
+        self.__createLift()
+        self.__createFloors()
+        self.__createPeople()
+
     def __createPeople(self) -> None:
+        '''
+        Creates people and distributes them to their floors.
+        '''
         for i in self.__requests:
             currentFloor = self.GetFloor(i)
             for j in i:
@@ -22,8 +29,15 @@ class building:
                 currentFloor.AddToPeople(newPerson)
 
     def __createFloors(self) -> None:
+        '''
+        Creates the floors and adds to the floors list.
+        '''
         for i in range(self.__numOfFloors):
             self.__floors.append(Floor(i))
     
     def __createLift(self) -> None:
+        '''
+        Creates the lift
+        '''
         self.__lift = lift(0,False,False,1,self.__capacity)
+

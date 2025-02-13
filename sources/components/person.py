6c23d4d91d5ea8f917a2f7d6dedd43a12fa9de55
marcos-ashton <marcosashiglesias@gmail.com>
2025-02-13 16:14:40 +0000

    deleted person

--- sources/components/person.py
+++
@@ -1,8 +0,0 @@
-#person component
-class Person:
-    def __init__(self, destination):
-        self.__destination = destination
-
-    def get_destination(self):
-        return self.__destination
-    

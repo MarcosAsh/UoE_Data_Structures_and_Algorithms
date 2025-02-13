db4240df00f26bcd64e016ac32c4b256cb2ef2f9
cw1169 <cw1169@exeter.ac.uk>
2025-02-13 15:43:24 +0000

    Push Commentd main_test

--- sources/main_test.py
+++ sources/main_test.py
@@ -55,10 +55,10 @@ Lift = lift(1, False, 1, lift_capacity)
 
 
 def mainloop():
-    while Lift.get_current_floor() < max_floors:
-        for waiting in requests[Lift.get_current_floor()]:
-            Lift.add_capacity()
-            print(Lift.get_capacity())
+    while Lift.get_current_floor() < max_floors: # while the lift is not at the top floor
+        for waiting in requests[Lift.get_current_floor()]: # for each person waiting on the current floor
+            Lift.add_capacity() # add the person to the lift
+            print(Lift.get_capacity()) # print the current capacity of the lift to check
 
 if __name__ == "__main__":
     mainloop()

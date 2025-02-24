2064d722bbb64eaef6b374ca3f5b483ecbcbb012
cw1169 <cw1169@exeter.ac.uk>
2025-02-24 16:17:07 +0000

    Pop first not working

--- sources/algorithms/stack.py
+++ sources/algorithms/stack.py
@@ -9,10 +9,6 @@ class stack:
     def pop(self):
         if not self.is_empty():
             return self.items.pop()  # Removes last item (LIFO)
-
-    def pop_first(self):  
-        if not self.is_empty():
-            return self.items.pop(0)  # Removes first item (FIFO)
     
     def peek(self):
         if not self.is_empty():

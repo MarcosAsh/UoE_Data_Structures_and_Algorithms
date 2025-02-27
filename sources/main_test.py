1caf1edccb9871974d9e25fbcb45a45bc13d8b6f
bbp202 <bbp202@exeter.ac.uk>
2025-02-27 17:44:22 +0000

    removed redundant code

--- sources/main_test.py
+++ sources/main_test.py
@@ -3,58 +3,6 @@ import os
 import time
 import threading
 
-# Ensure the sources directory is in the Python path
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "components")))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "algorithms")))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "input_files")))
-
-from components.building import building
-from algorithms.scan_algorithm import scan_algorithm_real_time
-from algorithms.look_algorithm import look_algorithm_real_time
-
-class InvalidInputError(Exception):
-    """Exception raised for invalid input."""
-    def __init__(self, message="Invalid input provided"):
-        super().__init__(message)
-
-def read_input_file(filename):
-    """Reads the input file and returns number of floors, lift capacity, and requests as a 2D array."""
-    num_floors = 0
-    lift_capacity = 0
-    requests = []
-
-    with open(filename, 'r') as file:
-            lines = file.readlines()
-
-            # Read number of floors and lift capacity
-            for line in lines:
-                line = line.strip()
-                if line.startswith("#") or not line:
-                    continue
-                if "," in line:
-                    try:
-                        parts = line.split(",")
-                        if len(parts) != 2 or not parts[0].strip():  #presence check for num_floors and capacity
-                            raise InvalidInputError("Invalid input provided. Line 2 in inputFile.")
-                        num_floors, lift_capacity = map(int, line.split(","))
-                    except(ValueError): #ensures both are integers
-                        raise InvalidInputError(message="Invalid input provided. Line 2 in input file, non-integer value.")
-                    requests = [[] for _ in range(num_floors)]  # Initialize a 2D array
-                    break
-
-            # Read floor requests
-            for line in lines:
-                line = line.strip()
-                if line.startswith("#") or not line:
-                    continue
-                if ":" in line:
-                    floor, destinations = line.split(":")
-                    floor = int(floor.strip()) - 1  # Convert floor to 0-based index
-                    if destinations.strip():
-                        requests[floor] = list(map(int, destinations.split(",")))
-    return num_floors, lift_capacity, requests
-
 # Assign variables from input file
 max_floors, lift_capacity, requests = read_input_file('sources/input_files/input0.txt')
 # Reverse Requests list

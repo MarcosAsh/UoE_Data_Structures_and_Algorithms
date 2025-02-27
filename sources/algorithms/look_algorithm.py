bd217290f1e42dd0767f00c90a9e4574b07c68cc
eddieallman <eoa207@exeter.ac.uk>
2025-02-27 21:29:32 +0000

    Completed the look algorithm

--- sources/algorithms/look_algorithm.py
+++ sources/algorithms/look_algorithm.py
@@ -27,56 +27,93 @@ def look_algorithm(building):
     print(building.get_remaining_people())
     remaining_people = building.get_remaining_people()
 
-    while remaining_people:
+    while remaining_people != 0:
+        for i in range(building.get_num_floors() - 1, -1, -1):
+            print(f"{i}: {building.get_floor(i).GetPeople().get_count()}")
+
         print(f"Current floor is {current_floor}")
         # Removing people from the lift
-        for person in lift.peopleList:
-            if person == current_floor:
-                lift.remove_people(person)
-                print(f"    Removed {person} from lift. Capacity: {lift.get_num_people()}")
+        for i in range(len(lift.peopleList) - 1, -1, -1):
+            if lift.peopleList[i] == current_floor:
+                print(f"    Removed {lift.peopleList[i]} from lift. Capacity: {lift.get_num_people()}")
+                lift.remove_people(lift.peopleList[i])
 
         # Adding people onto the lift if capacity allows
-        floor_queue = building.get_floor(current_floor).GetPeople()
-        while lift.get_num_people() <= lift.get_capacity():
-            person = floor_queue.dequeue()
-            lift.add_people(person)
-            print(f"    Added {person} to lift. Capacity: {lift.get_num_people()}")
+        while lift.get_num_people() < lift.get_capacity():
+            person = building.get_floor(current_floor).RemoveFromPeople()
+
+            if person is None:
+                break
+            else:
+                lift.add_people(person)
+                print(f"    Added {person} to lift. Capacity: {lift.get_num_people()}")
                     
         # Finding the next request or call (if capacity allows) in the current direction
         # Sort the requests in the elevator that are along the current direction
-        print(f"    People in lift: {lift.people_list}, Current direction: {direction}")
+        print(f"    People in lift: {lift.peopleList}, Current direction: {direction}")
+        moving = True
         if direction == 1:
-            next_requests = [req for req in lift.people_list if req >= current_floor]
+            next_requests = [req for req in lift.peopleList if req >= current_floor]
             next_requests = quicksort(next_requests)
-            
-            # Select the closest request
-            next_request = next_requests[0]
-            print(f"    Next floor: {next_request}")
-            if not lift.get_num_people() <= lift.get_capacity():
-                # Check for calls in between the current floor and the next request
-                for i in range(current_floor, next_requests[0] + 1):
-                    if building.get_floor(i).GetPeople():
-                        next_request = i
-                        print(f"    New call found at {next_request}")
-                        break
+            if next_requests:
+                # Select the closest request
+                next_request = next_requests[0]
+                print(f"    Next floor going up: {next_request}")
+                if lift.get_num_people() < lift.get_capacity():
+                    # Check for calls in between the current floor and the next request
+                    for i in range(current_floor, next_requests[0] + 1):
+                        if building.get_floor(i).GetPeople().get_count() != 0:
+                            next_request = i
+                            print(f"    New call found at {next_request}")
+                            break
+            elif lift.get_num_people() < lift.get_capacity():
+                next_request = None
+                for i in range(current_floor, building.get_num_floors()):
+                    if building.get_floor(i).GetPeople().get_count() != 0:
+                            next_request = i
+                            print(f"    New call found at {next_request}")
+                            break
+                if next_request is None:
+                    direction = -1
+            else:
+                direction = -1
         if direction == -1:
-            next_requests = [req for req in lift.people_list if req <= current_floor]
+            next_requests = [req for req in lift.peopleList if req <= current_floor]
+            print(next_requests)
             next_requests = quicksort(next_requests)
-            
-            # Select the closest request
-            next_request = next_requests[-1]
-            print(f"    Next floor: {next_request}")
-            if not lift.get_num_people() <= lift.get_capacity():
-                # Check for calls in between the current floor and the next request
-                for i in range(next_requests[-1] + 1, current_floor, -1):
-                    if building.getFloor(i).GetPeople():
+            if next_requests:
+                # Select the closest request
+                next_request = next_requests[-1]
+                print(f"    Next floor going down: {next_request}")
+                if lift.get_num_people() < lift.get_capacity():
+                    # Check for calls in between the current floor and the next request
+                    for i in range(current_floor, next_requests[-1], -1):
+                        print(i)
+                        if building.get_floor(i).GetPeople().get_count() != 0:
+                            next_request = i
+                            print(f"    New call found at {next_request}")
+                            break
+            elif lift.get_num_people() < lift.get_capacity():
+                next_request = None
+                for i in range(current_floor, -1, -1):
+                    if building.get_floor(i).GetPeople().get_count() != 0:
                         next_request = i
                         print(f"    New call found at {next_request}")
                         break
+                if next_request is None:
+                    direction = 1
+                    moving = False
+            else:
+                direction = 1
+                moving = False
 
         # Going to that floor
-        time.sleep(0.2)
-        current_floor = next_request
+        if moving:
+            time.sleep(0.2)
+            current_floor = next_request
+        
+        remaining_people = building.get_remaining_people()
+        print(remaining_people)
 
 # Test code
 if __name__ == "__main__":

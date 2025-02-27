226f26fa75d601dd98c28d58e87b1d388066e2e9
cw1169 <cw1169@exeter.ac.uk>
2025-02-27 17:56:40 +0000

    fixed building case and added get_num_floors

--- sources/algorithms/my_lift_algorithm.py
+++ sources/algorithms/my_lift_algorithm.py
@@ -17,50 +17,20 @@ Building = building(num_floors, lift_capacity, requests)
 # Algorithm that moves the lift to the next closest request and can move up or down
 def mylift(Building):
     Lift = Building.getLift()
-    while Building.getRemainingPeople() > 0:
+    remaining_people = Building.getRemainingPeople()
+    num_floors = building.__building__numOfFloors 
+
+    all_requests = []
+
+    for i in range(num_floors):
+        all_requests.extend(building.getFloor(i).GetPeople())
+        
+    while remaining_people > 0:
         # Get the current floor of the lift
         current_floor = Lift.get_current_floor()
-        # Get the list of people on the current floor
-        people = Building.getFloor(current_floor).GetPeople()
-        # Get the number of people on the current floor
-        num_people = Building.getFloor(current_floor).GetNumPeople()
-        # Get the direction the lift is moving
-        direction = Lift.get_move()
-        # Check if there are people on the current floor
-        if num_people > 0:
-            # Check if the lift is moving up
-            if direction == 1:
-                # Check if the lift is not at the top floor
-                if current_floor < Building.__numOfFloors - 1:
-                    # Move the lift up
-                    Lift.change_current_floor(current_floor + 1)
-                    print(f"Lift moving up to floor {current_floor + 1}")
-                else:
-                    # Move the lift down
-                    Lift.move_down()
-                    print(f"Lift moving down to floor {current_floor - 1}")
-            # Check if the lift is moving down
-            elif direction == -1:
-                # Check if the lift is not at the bottom floor
-                if current_floor > 0:
-                    # Move the lift down
-                    Lift.change_current_floor(current_floor - 1)
-                    print(f"Lift moving down to floor {current_floor - 1}")
-                else:
-                    # Move the lift up
-                    Lift.move_up()
-                    print(f"Lift moving up to floor {current_floor + 1}")
-        else:
-            # Check if the lift is moving up
-            if direction == 1:
-                # Move the lift up
-                Lift.change_current_floor(current_floor + 1)
-                print(f"Lift moving up to floor {current_floor + 1}")
-            # Check if the lift is moving down
-            elif direction == -1:
-                # Move the lift down
-                Lift.change_current_floor(current_floor - 1)
-                print(f"Lift moving down to floor {current_floor - 1}")
+        # Get the next closest request
+        print(all_requests)
+
 
 if __name__ == '__main__':
     mylift(Building)

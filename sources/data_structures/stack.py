b5e748b0d222f0afdad09b4394c3dc417b47acd3
marcos-ashton <marcosashiglesias@gmail.com>
2025-02-27 17:46:59 +0000

    commented stack.py

--- sources/data_structures/stack.py
+++ sources/data_structures/stack.py
@@ -2,21 +2,25 @@
 class Stack:
     def __init__(self):
         self.__items = []
-
+    # add to the top element of the stack
     def push(self, item):
         self.__items.append(item)
 
+    # remove from the top element of the stack
     def pop(self):
         if not self.is_empty():
             return self.__items.pop()
 
+    # to get the top element of the stack
     def peek(self):
         if not self.is_empty():
             return self.__items[-1]
         return None
 
+    # to check if the stack is empty
     def is_empty(self):
         return len(self.__items) == 0
 
+    # size of stack
     def size(self):
         return len(self.__items)

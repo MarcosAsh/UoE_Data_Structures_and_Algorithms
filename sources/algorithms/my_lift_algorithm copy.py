c904ecfa15448609ecdeb4b267b5b8dd2edfab86
cw1169 <cw1169@exeter.ac.uk>
2025-02-28 04:39:42 +0000

    my_lift_algorithm complete without seek_count

--- sources/algorithms/my_lift_algorithm copy.py
+++ sources/algorithms/my_lift_algorithm copy.py
@@ -28,32 +28,34 @@ def my_lift(Building):
     Lift = Building.get_lift()
     num_floors = Building.get_num_floors() 
 
-    next_requested_floor_up = 0
-    next_requested_floor_down = 0
+    next_requested_floor_up = None
+    next_requested_floor_down = None
 
     while Building.get_remaining_people() > 0:
 
         current_floor = Lift.get_current_floor()
+        target_floor = None
 
         # If lift is not full
         if Lift.get_num_people() < lift_capacity:
             # Get the next closest request up
             # Start at floor above current, end at max floor, setting i as index/floor
             for i in range(current_floor + 1, num_floors):
-                checking_floor = Building.get_floor(i)
-                if checking_floor.GetNumPeople() > 0:
+                if  Building.get_floor(i).GetPeople().get_count() > 0:
                     next_requested_floor_up = i
+                    break
+            # print('next_requested_floor_up = ', next_requested_floor_up)
             
-            if next_requested_floor_up > num_floors:
+            if next_requested_floor_up is not None and next_requested_floor_up >= num_floors - 1:
                 next_requested_floor_up = None
 
             # Get the next closest request down
-            for i in range(current_floor - 1, 0, -1):
-                checking_floor = Building.get_floor(i)
-                if checking_floor.GetNumPeople() > 0:
+            for i in range(current_floor -1, -1, -1):
+                if Building.get_floor(i).GetPeople().get_count() > 0:
                     next_requested_floor_down = i
+                    break
             
-            if next_requested_floor_down < 0:
+            if next_requested_floor_down is not None and next_requested_floor_down <= 0:
                 next_requested_floor_down = None
             
             # If both dont exist
@@ -74,46 +76,41 @@ def my_lift(Building):
                 target_floor = next_requested_floor_down
             # Else same
             else:
-                target_floor = next_requested_floor_up
-        # If lift is full
-        else:
-            for request in Lift.peopleList:
-                if request > current_floor:
-                    pass
-                elif request < current_floor:
-                    pass
-                # Same as current floor
+                if Building.get_floor(next_requested_floor_up).GetPeople().get_count() < Building.get_floor(next_requested_floor_down).GetPeople().get_count():
+                    target_floor = next_requested_floor_down
                 else:
-                    pass
-        # Move the lift to the next closest request
-        if up_count < down_count:
-            Lift.move_up()
-            Lift.change_current_floor(next_request_up)
-            requests_pointer[next_request_up] = 0
-        if down_count < up_count:
-            Lift.move_down()
-            Lift.change_current_floor(next_request_down)
-            requests_pointer[next_request_down] = 0
-        else:
-            if Lift.get_move() == 1:
-                Lift.change_current_floor(next_request_up)
-                requests_pointer[next_request_up] = 0
-            else:
-                Lift.change_current_floor(next_request_down)
-                requests_pointer[next_request_down] = 0
+                    target_floor = next_requested_floor_up
 
-        # add floor to seek sequence
-        seek_sequence.append(Lift.get_current_floor())
+        # If lift is full
+        elif Lift.get_num_people() == lift_capacity:
+            min_distance = 999
+            for request in Lift.peopleList:
+                # print(request)
+                if abs(request - current_floor) < min_distance:
+                    min_distance = abs(request - current_floor)
+                    print('min_distance = ', min_distance, 'request = ', request)
+                    target_floor = request
+            
 
-        # get the vacancy of the lift
-        vacancy = Lift.get_capacity() - Lift.get_num_people()
+            # Check if the target floor is the current floor
+        if target_floor is not None and target_floor != current_floor:
+            Lift.change_current_floor(target_floor)
+            print(f"Lift moving to floor {Lift.get_current_floor()}")
+
 
         # remove people from the lift if they are on the current floor
+        people_to_remove = []
         for person in Lift.peopleList:
             if person == Lift.get_current_floor():
-                Lift.remove_people(person)
+                people_to_remove.append(person)
 
-        # Adding people onto the lift if capacity allows
+        for person in people_to_remove:
+            Lift.remove_people(person)
+            print(f"Removed {person} from lift. Capacity: {Lift.get_num_people()}")
+
+        
+
+        # add people onto the lift if capacity allows
         while Lift.get_num_people() < Lift.get_capacity():
             person = Building.get_floor(Lift.get_current_floor()).RemoveFromPeople()
             # Making sure the item is not added if the queue has nothing in it and returns None
@@ -129,11 +126,9 @@ def my_lift(Building):
                 print(Lift.peopleList)
                 # print remaining people in the building
                 print(Building.get_remaining_people())
-                print(requests_pointer)
- 
-    return seek_sequence
-
-
+                # print length of floor queue
+                print('------------------------------------')
+    return None
 if __name__ == '__main__':
-    print(mylift(Building))
+    print(my_lift(Building))
     
